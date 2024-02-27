ff68c9d3737a97dbbf1664160456c63439b5ca7f
Camila Araujo <camila.araujo.rp@hotmail.com>
2024-02-27 13:49:56 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.18.3/dist/css/uikit.min.css" />
+
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.18.3/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.18.3/dist/js/uikit-icons.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Página</title>
+</head>
+
+<body>
+    <header>
+        <div style="background-color: black; color: white;">
+            <div class="uk-container" uk-grid>
+                <div class="uk-width-1-3">
+                    testandoo
+                </div>
+                
+
+            </div>
+            teste
+        </div>
+    </header>
+    <div class="uk-container">
+        <h1>Olá, mundo!</h1>
+        <p>Esta é uma página PHP básica.</p>
+
+        <?php
+        // Exemplo de código PHP
+        $nome = "João";
+        echo "<p>Oi, $nome! Bem-vindo à minha página PHP.</p>";
+        ?>
+    </div>
+</body>
+
+</html>
