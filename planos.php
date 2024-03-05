bb455f287ba03943b96c12591c3675df4e8f8456
Camila Araujo <camila.araujo.rp@hotmail.com>
2024-03-05 14:39:25 -0300

    inicio de planos

--- planos.php
+++ planos.php
@@ -4,7 +4,394 @@ include 'header.php';
 
 ?>
 
+<div style="text-align: center;">
+    <br>
+    <h1 class="titulo-verde" uk-scrollspy="cls: uk-animation-slide-top; repeat: true"> NOSSOS PLANOS </h1>
+</div>
 
+<div class="uk-container">
+
+    <div class="uk-child-width-1-3@m" uk-grid>
+        <div uk-scrollspy="cls: uk-animation-slide-left; repeat: true">
+            <div class="uk-card uk-card-default uk-card-body card-planos-prata">
+                <h3 class=" titulo-card-plano">PRATA</h3>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Landing Page (1 pagina)</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Contato por WhatsApp</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/cancelar.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Lista E-mail marketing</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/cancelar.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Suporte</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/cancelar.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Alterações de imagens e texto</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/cancelar.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Tráfego pago</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/cancelar.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Manutenção</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/cancelar.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Banner mensal</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/cancelar.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Desconto em alterações complexas</p>
+                    </div>
+                </div><BR><BR>
+
+
+                <div class="linha-planos"> </div>
+                <h4 class="texto-investir ">Investir por apenas: <br> <span class="texto-destaque"> R$ 999,99 </span> </h4>
+                <div class="linha-planos"> </div>
+            </div>
+
+            <div style="text-align: center;" uk-scrollspy="cls: uk-animation-shake; repeat: true">
+                <br>
+                <button class="uk-button botao-fundo-escuro">SAIBA MAIS</button>
+            </div>
+        </div>
+
+
+
+        <div uk-scrollspy="cls: uk-animation-slide-top; repeat: true">
+            <div class="uk-card uk-card-default uk-card-body card-planos-ouro">
+                <h3 class="titulo-card-plano">OURO</h3>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Site institucional (3 paginas)</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Contato por WhatsApp</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Lista E-mail marketing</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> 3 meses de suporte</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> 3 meses de alterações de imagens e texto</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/cancelar.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Tráfego pago</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/cancelar.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Manutenção</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/cancelar.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Banner mensal</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/cancelar.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Desconto em alterações complexas</p>
+                    </div>
+                </div>
+                <BR>
+
+                <div class="linha-planos"> </div>
+                <h4 class="texto-investir ">Investir por apenas: <br> <span class="texto-destaque"> R$ 1.499,99 </span> </h4>
+                <div class="linha-planos"> </div>
+            </div>
+
+            <div style="text-align: center;" uk-scrollspy="cls: uk-animation-shake; repeat: true">
+                <br>
+                <button class="uk-button botao-fundo-escuro">SAIBA MAIS</button>
+            </div>
+        </div>
+
+
+
+        <div uk-scrollspy="cls: uk-animation-slide-right; repeat: true">
+            <div class="uk-card uk-card-default uk-card-body card-planos-platinum">
+                <h3 class=" titulo-card-plano">PLATINUM</h3>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos">Site institucional (5 páginas)</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Contato por WhatsApp</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Lista E-mail marketing</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> 6 meses de suporte</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> 6 meses de alterações de imagens e texto</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> 6 meses de tráfego pago</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> 6 meses de manutenção</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> 6 meses de banner mensal</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> 6 meses de desconto em alterações complexas</p>
+                    </div>
+                </div>
+
+                <div class="linha-planos"> </div>
+                <h4 class="texto-investir ">Investir por apenas: <br> <span class="texto-destaque"> R$ 1.899,99 </span> </h4>
+                <div class="linha-planos"> </div>
+            </div>
+
+            <div style="text-align: center;" uk-scrollspy="cls: uk-animation-shake; repeat: true">
+                <br>
+                <button class="uk-button botao-fundo-escuro">SAIBA MAIS</button>
+            </div>
+
+        </div>
+
+
+
+    </div>
+
+
+    <br>
+    <div class="uk-flex uk-flex-center" uk-scrollspy="cls: uk-animation-slide-bottom; repeat: true">
+
+
+
+        <div>
+            <div class="uk-card uk-card-default uk-card-body card-planos-suporte">
+                <h3 class="titulo-card-plano">ADICIONAL MENSAL <br> PLANO SUPORTE</h3>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Landing page simples (1 pagina)</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Contato por WhatsApp</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Lista E-mail marketing</p>
+                    </div>
+                </div>
+                <div class="uk-flex uk-flex-middle">
+                    <div style="padding-right: 5px;">
+                        <img src="assets/imagens/check.png" width="20" alt="Vantagens" class="imagem-vantagem">
+                    </div>
+                    <div style="margin-top: 20px;">
+
+                        <p class="texto-card-planos"> Suporte</p>
+                    </div>
+                </div>
+
+
+                <div class="linha-planos"> </div>
+                <h4 class="texto-investir ">Investir por apenas: <br> <span class="texto-destaque"> R$ 249,99 </span> </h4>
+                <div class="linha-planos"> </div>
+            </div>
+
+            <div style="text-align: center;" uk-scrollspy="cls: uk-animation-shake; repeat: true">
+                <br>
+                <button class="uk-button botao-fundo-escuro">SAIBA MAIS</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<br>
+<div style="background-color: #004250;">
+    <div class="uk-container">
+        <div style="text-align: center;">
+            <br>
+            <h2 class="titulo" uk-scrollspy="cls: uk-animation-slide-top; repeat: true"> Porque ser Platinum? </h2>
+            <br>
+        </div>
+        <div class="uk-grid">
+
+            <div class="uk-width-1-3">
+
+            </div>
+
+            <div class="uk-width-2-3">
+
+            </div>
+
+        </div>
+    </div>
+</div>
 <?php
 
 include 'footer.php';
