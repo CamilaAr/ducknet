dd14e4f6bbea8a15d169f543e380d1f700feaacb
Camila Araujo <camila.araujo.rp@hotmail.com>
2024-03-06 14:44:49 -0300

    finalização do FAQ

--- faq.php
+++ faq.php
@@ -3,7 +3,40 @@
 include 'header.php';
 
 ?>
+<div class="uk-container">
+    <div style="text-align: center;">
+        <br>
+        <h1 class="titulo-verde" uk-scrollspy="cls: uk-animation-slide-top; repeat: true"> FAQ - PERGUNTAS FREQUENTES </h1>
+        <br>
+    </div>
 
+</div>
+
+<div class="uk-container uk-margin-top">
+    <ul uk-accordion>
+        <li>
+            <a  class="uk-accordion-title pergunta" href>uis aute irure dolor in reprehenderit in voluptate? </a>
+            <div style="border-radius: 20px;"  class="uk-accordion-content uk-box-shadow-large  uk-padding ">
+                <p >Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
+            </div>
+        </li>
+        <li>
+            <a  class="uk-accordion-title pergunta" href>uis aute irure dolor in reprehenderit in voluptate? </a>
+            <div style="border-radius: 20px;"  class="uk-accordion-content uk-box-shadow-large  uk-padding ">
+                <p >Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
+            </div>
+        </li>
+        <li>
+            <a  class="uk-accordion-title pergunta" href>uis aute irure dolor in reprehenderit in voluptate? </a>
+            <div style="border-radius: 20px;" class="uk-accordion-content uk-box-shadow-large  uk-padding ">
+                <p >Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
+            </div>
+        </li>
+    </ul>
+
+
+</div>
+<br><br>
 
 <?php
 
